081be60d23d9a713e585356e592ef1049f4b280c
HendersonMentum <henderson.villegas@mentum.group>
2023-08-30 01:27:01 +0000

    add changes

--- qp_dynamic_taxes_and_charges/qp_dynamic_taxes_and_charges/overrides/overrided_doctypes.py
+++ qp_dynamic_taxes_and_charges/qp_dynamic_taxes_and_charges/overrides/overrided_doctypes.py
@@ -5,6 +5,9 @@ from erpnext.selling.doctype.quotation.quotation import Quotation
 from erpnext.accounts.doctype.purchase_invoice.purchase_invoice import PurchaseInvoice
 from erpnext.buying.doctype.purchase_order.purchase_order import PurchaseOrder
 from erpnext.accounts.doctype.payment_entry.payment_entry import PaymentEntry
+from frappe.desk.doctype.workspace.workspace import Workspace
+from frappe.model.naming import parse_naming_series
+import frappe 
 
 class CustomSalesInvoice(SalesInvoice):
 	def calculate_taxes_and_totals(self):
@@ -17,6 +20,13 @@ class CustomSalesInvoice(SalesInvoice):
 			self.calculate_commission()
 			self.calculate_contribution()
 	
+	def autoname(self):
+
+		if frappe.db.get_single_value('Autoname File Config', "autoname"):
+			name = self.naming_series + self.sequence
+			parts = name.split('.')
+			self.name = parse_naming_series(parts)
+
 class CustomSalesOrder(SalesOrder):
 	def calculate_taxes_and_totals(self):
 
@@ -26,7 +36,7 @@ class CustomSalesOrder(SalesOrder):
 
 		if self.doctype in ["Quotation", "Sales Order", "Delivery Note", "Sales Invoice"]:
 			self.calculate_commission()
-			self.calculate_contribution()
+			self.calculate_contribution() 
 
 class CustomDeliveryNote(DeliveryNote):
 	def calculate_taxes_and_totals(self):
@@ -70,7 +80,8 @@ class CustomPurchaseOrder(PurchaseOrder):
 
 		if self.doctype in ["Quotation", "Sales Order", "Delivery Note", "Sales Invoice"]:
 			self.calculate_commission()
-			self.calculate_contribution()
+			self.calculate_contribution() 
+
 
 class CustomPaymentEntry(PaymentEntry):
 	def calculate_taxes_and_totals(self):
@@ -82,3 +93,39 @@ class CustomPaymentEntry(PaymentEntry):
 		if self.doctype in ["Quotation", "Sales Order", "Delivery Note", "Sales Invoice"]:
 			self.calculate_commission()
 			self.calculate_contribution()
+
+class CustomWorkspace(Workspace):
+
+	def get_link_groups(self):
+		cards = []
+		current_card = {
+			"label": "Link",
+			"type": "Card Break",
+			"icon": None,
+			"hidden": False,
+		}
+
+		card_links = []
+
+		for link in self.links:
+			link = link.as_dict()
+			
+			if link.type == "Card Break":
+				if card_links and (not current_card.only_for or current_card.only_for == frappe.get_system_settings('country')): 
+					current_card['links'] = card_links
+					cards.append(current_card)
+
+				current_card = link
+				card_links = []
+			else:
+				card_links.append(link)
+
+		current_card['links'] = card_links
+		
+		if self.extends:
+			if current_card['label'] == 'Link':
+				current_card['label'] = self.cards_label if self.cards_label else 'Link'
+
+		cards.append(current_card)
+
+		return cards
