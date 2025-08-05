b4c6e5287fc2e066ff0e574400716e2042639c5d
HendersonMentum <henderson.villegas@mentum.group>
2025-08-05 16:29:07 +0000

    add changes to taxes_and_charges calculate taxes merge

--- qp_dynamic_taxes_and_charges/public/js/controllers/transaction_fun.js
+++ qp_dynamic_taxes_and_charges/public/js/controllers/transaction_fun.js
@@ -1817,113 +1817,102 @@ erpnext.TransactionController = erpnext.taxes_and_totals.extend({
 
 	taxes_and_charges: function() {
 		var me = this;
-    
-		frappe.call({
-			method: "qp_dynamic_taxes_and_charges.qp_dynamic_taxes_and_charges.services.taxes.get_taxes_config",
-			args: {
-				"field":"cruzar_impuestos"
-			},
-			async: false,
-			callback: function(r) {
-				
 
-				if(me.frm.doc.taxes_and_charges) {
+		if(me.frm.doc.taxes_and_charges) {
 
-					let is_check_merge = r.message
-					let master_doctype = frappe.meta.get_docfield(me.frm.doc.doctype, "taxes_and_charges", me.frm.doc.name).options
-					let master_name = me.frm.doc.taxes_and_charges
-				
-					return frappe.call({
-						method: "erpnext.controllers.accounts_controller.get_taxes_and_charges",
-						args: {
-							"master_doctype": master_doctype,
-							"master_name": master_name
-						},
-						callback: function(r) {
-							if(!r.exc) {
-		
-								if(me.frm.doc.shipping_rule && me.frm.doc.taxes) {
-										
-										let tax_list = me.frm.doc.items.filter(i => i.item_tax_template).map(i => i.item_tax_template)
-		
-										for (let tax of r.message) {
-											if(is_check_merge && !['Purchase Order', 'Purchase Invoice', 'Purchase Receipt'].includes(me.frm.doc.doctype)){
-												if(tax_list.length > 0){
-													if(tax_exists(master_name, tax.account_head, tax.rate)){
-														
-														let add_tax = true;
+			let is_check_merge = cint(frappe.boot.cruzar_impuestos);
+			let master_doctype = frappe.meta.get_docfield(me.frm.doc.doctype, "taxes_and_charges", me.frm.doc.name).options
+			let master_name = me.frm.doc.taxes_and_charges
 
-														if(['On Previous Row Amount', 'Previous Row Total'].includes(tax.charge_type)){
-															
-															let prev_account_head = r.message[tax.row_id -1].account_head
-															let prev_rate = r.message[tax.row_id -1].rate
+			return frappe.call({
+				method: "erpnext.controllers.accounts_controller.get_taxes_and_charges",
+				args: {
+					"master_doctype": master_doctype,
+					"master_name": master_name
+				},
+				callback: function(r) {
+					if(!r.exc) {
 
-															if(!tax_exists(tax_list, prev_account_head, prev_rate)){
-																add_tax = false;
-															}     
-															
-															tax.row_id = me.frm.doc.taxes.slice(-1).idx; 
-														}
+						if(me.frm.doc.shipping_rule && me.frm.doc.taxes) {
 
-														if(add_tax){
-															me.frm.add_child("taxes", tax);
-														}
+								let tax_list = me.frm.doc.items.filter(i => i.item_tax_template).map(i => i.item_tax_template)
+
+								for (let tax of r.message) {
+									if(is_check_merge && !['Purchase Order', 'Purchase Invoice', 'Purchase Receipt'].includes(me.frm.doc.doctype)){
+										if(tax_list.length > 0){
+											if(tax_exists(master_name, tax.account_head, tax.rate)){
+
+												let add_tax = true;
+
+												if(['On Previous Row Amount', 'Previous Row Total'].includes(tax.charge_type)){
+
+													let prev_account_head = r.message[tax.row_id -1].account_head
+													let prev_rate = r.message[tax.row_id -1].rate
+
+													if(!tax_exists(tax_list, prev_account_head, prev_rate)){
+														add_tax = false;
 													}
+
+													tax.row_id = me.frm.doc.taxes.slice(-1).idx;
 												}
-												
-											}else{
-												me.frm.add_child("taxes", tax);
+
+												if(add_tax){
+													me.frm.add_child("taxes", tax);
+												}
 											}
 										}
-												
-									refresh_field("taxes");
-								} else {
-									if(is_check_merge && !['Purchase Order', 'Purchase Invoice', 'Purchase Receipt'].includes(me.frm.doc.doctype)){
-										
-											let tax_list = me.frm.doc.items.filter(i => i.item_tax_template).map(i => i.item_tax_template)
-		
-											if(tax_list.length > 0){
-												me.frm.set_value("taxes", []);
-		
-												for (let tax of r.message) {
-													if(tax_exists(tax_list, tax.account_head, tax.rate)){
 
-														let add_tax = true;
-
-														if(['On Previous Row Amount', 'Previous Row Total'].includes(tax.charge_type)){
-															
-															let prev_account_head = r.message[tax.row_id -1].account_head
-															let prev_rate = r.message[tax.row_id -1].rate
-
-															if(!tax_exists(tax_list, prev_account_head, prev_rate)){
-																add_tax = false;
-															}
-															
-															tax.row_id = me.frm.doc.taxes.slice(-1).idx;               
-														}
-
-														if(add_tax){
-															me.frm.add_child("taxes", tax);
-														}
-														
-													}
-												}
-											}
-										
-											
-									
 									}else{
-										me.frm.set_value("taxes", r.message);
+										me.frm.add_child("taxes", tax);
 									}
-			
-									me.calculate_taxes_and_totals();
 								}
+
+							refresh_field("taxes");
+						} else {
+							if(is_check_merge && !['Purchase Order', 'Purchase Invoice', 'Purchase Receipt'].includes(me.frm.doc.doctype)){
+
+									let tax_list = me.frm.doc.items.filter(i => i.item_tax_template).map(i => i.item_tax_template)
+
+									if(tax_list.length > 0){
+										me.frm.set_value("taxes", []);
+
+										for (let tax of r.message) {
+											if(tax_exists(tax_list, tax.account_head, tax.rate)){
+
+												let add_tax = true;
+
+												if(['On Previous Row Amount', 'Previous Row Total'].includes(tax.charge_type)){
+
+													let prev_account_head = r.message[tax.row_id -1].account_head
+													let prev_rate = r.message[tax.row_id -1].rate
+
+													if(!tax_exists(tax_list, prev_account_head, prev_rate)){
+														add_tax = false;
+													}
+
+													tax.row_id = me.frm.doc.taxes.slice(-1).idx;
+												}
+
+												if(add_tax){
+													me.frm.add_child("taxes", tax);
+												}
+
+											}
+										}
+									}
+
+
+
+							}else{
+								me.frm.set_value("taxes", r.message);
 							}
+
+							me.calculate_taxes_and_totals();
 						}
-					});
+					}
 				}
-			}
-		});
+			});
+		}
 	},
 
 	tax_category: function() {
