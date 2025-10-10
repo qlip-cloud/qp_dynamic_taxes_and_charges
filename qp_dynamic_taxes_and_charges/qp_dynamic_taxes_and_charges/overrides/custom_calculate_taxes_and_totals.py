313475cc173602c0aa52d788fc9f5c8ef33721de
HendersonMentum <henderson.villegas@mentum.group>
2025-10-10 22:06:02 +0000

    ADD CHANGES TO INDIVIDUAL TAX ON PURCHASE

--- qp_dynamic_taxes_and_charges/qp_dynamic_taxes_and_charges/overrides/custom_calculate_taxes_and_totals.py
+++ qp_dynamic_taxes_and_charges/qp_dynamic_taxes_and_charges/overrides/custom_calculate_taxes_and_totals.py
@@ -8,9 +8,10 @@ class custom_calculate_taxes_and_totals(calculate_taxes_and_totals):
     def _get_tax_rate(self, tax, item_tax_map):
             
         impuesto_individual = frappe.db.get_single_value('Dynamic Taxes Config', "impuesto_individual")
+        cruce_de_impuestos_en_compras = frappe.db.get_single_value('Dynamic Taxes Config', "cruce_de_impuestos_en_compras")
 
         if tax.account_head in item_tax_map:
-            if impuesto_individual:
+            if impuesto_individual and (True if cruce_de_impuestos_en_compras else  self.doc.doctype not in ["Purchase Invoice", "Purchase Order", "Purchase Receipt"]):
                 if tax.charge_type in ['On Previous Row Amount', 'Previous Row Total'] and not item_tax_map.get(self.doc.get("taxes")[cint(tax.row_id) - 1].account_head):
                     if tax.charge_type == 'Previous Row Total':
                         self.doc.get("taxes")[cint(tax.row_id) - 1].grand_total_for_current_item = self.doc.get("taxes")[cint(tax.row_id) - 1].tax_amount
@@ -19,7 +20,7 @@ class custom_calculate_taxes_and_totals(calculate_taxes_and_totals):
 
             return flt(item_tax_map.get(tax.account_head), self.doc.precision("rate", tax))
         else:
-            if impuesto_individual:
+            if impuesto_individual and (True if cruce_de_impuestos_en_compras else  self.doc.doctype not in ["Purchase Invoice", "Purchase Order", "Purchase Receipt"]):
                 if tax.charge_type in ['On Previous Row Amount', 'Previous Row Total'] and not item_tax_map.get(self.doc.get("taxes")[cint(tax.row_id) - 1].account_head):
                     if tax.charge_type == 'Previous Row Total':
                          self.doc.get("taxes")[cint(tax.row_id) - 1].grand_total_for_current_item = self.doc.get("taxes")[cint(tax.row_id) - 1].tax_amount
@@ -33,8 +34,9 @@ class custom_calculate_taxes_and_totals(calculate_taxes_and_totals):
     def _load_item_tax_rate(self, item_tax_rate):
 
         cruzar_impuestos = frappe.db.get_single_value('Dynamic Taxes Config', "cruzar_impuestos")
-
-        if cruzar_impuestos:
+        cruce_de_impuestos_en_compras = frappe.db.get_single_value('Dynamic Taxes Config', "cruce_de_impuestos_en_compras")
+        
+        if cruzar_impuestos and (True if cruce_de_impuestos_en_compras else  self.doc.doctype not in ["Purchase Invoice", "Purchase Order", "Purchase Receipt"]):
             if item_tax_rate:
                 return_item_tax_rate = json.loads(item_tax_rate)
                 return_item_tax_rate_copy = return_item_tax_rate.copy()
